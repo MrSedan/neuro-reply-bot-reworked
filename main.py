68db02c50405090109247ff5fa8c3a753af0c97e
Sergey Elpashev <elpashevs@bk.ru>
2023-11-04 02:24:10 +0300

    Handlers autoloading from handlers folder

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import asyncio
 import logging
 import os
 import sys
+from os.path import dirname, join
 
 import aioschedule as schedule
 import dotenv
@@ -24,8 +25,17 @@ dp = Dispatcher()
 async def start_message(message: types.Message):
     await message.answer('Абоба')
 
+handlers_dir = join(dirname(__file__), 'handlers')
+
+for filename in os.listdir(handlers_dir):
+    if filename.endswith('.py'):
+        module_name = filename[:-3]
+        setup = __import__(f"handlers.{module_name}", locals(), globals(), ['setup']).setup
+        dp.include_router(setup(bot))
+
+
 async def main() -> None:
-    dp.include_router(Admin_commands(bot)())
+    # dp.include_router(Admin_commands(bot)())
     await dp.start_polling(bot, skip_updates=True)
     
 if __name__ == '__main__':
