3a7e80adcbe45825195087b88f242fe949fcb008
Sergey Elpashev <elpashevs@bk.ru>
2023-11-04 05:32:00 +0300

    Admin check

--- handlers/middlewares/user.py
+++ handlers/middlewares/user.py
@@ -4,9 +4,8 @@ from aiogram import BaseMiddleware
 from aiogram.types import Message
 from sqlalchemy.orm import Session
 
-from db.data import User, engine
+from db.data import Admin, User, engine
 
-ADMIN_LIST = [248770879, 395543883]
 
 class AdminMiddleware(BaseMiddleware):
     def __init__(self) -> None:
@@ -14,11 +13,12 @@ class AdminMiddleware(BaseMiddleware):
     
     async def __call__(self, handler: Callable[[Message, Dict[str, Any]], Awaitable[Any]], event: Message, data: Dict[str, Any]) -> Any:
         with Session(engine) as session:
-                if not session.get(User, event.from_user.id):
-                    user = User(id=event.from_user.id, user_name=event.from_user.username)
-                    session.add(user)
-                    session.commit()
-        if event.from_user.id not in ADMIN_LIST:
+            if not session.get(User, event.from_user.id):
+                user = User(id=event.from_user.id, user_name=event.from_user.username)
+                session.add(user)
+                session.commit()
+            isAdmin = session.get(Admin, event.from_user.id)
+        if not isAdmin:
             await event.answer('Команда только для админов!')
             return None
         return await handler(event, data)
